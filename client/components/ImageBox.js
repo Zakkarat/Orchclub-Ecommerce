6cf0d6b4e91aadd29f74250a74fca83edd404892
Alexander Yakubovskiy <35102255+Zakkarat@users.noreply.github.com>
2020-03-05 09:17:25 +0200

    nav component

---
+++ client/components/ImageBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MDBCol } from 'mdbreact'
+
+const ImageBox = () => {
+    return (
+            <MDBCol md="5">
+                1
+            </MDBCol>
+    )
+}
+
+export default ImageBox
