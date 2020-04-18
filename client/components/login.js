d9bc9b1153da35d5770b3506054f6c499bbc6ea3
Zakkaray <bloomzakk@gmail.com>
2020-04-18 17:19:22 +0300

    front auth added

---
+++ client/components/login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCol,
+  MDBIcon,
+  MDBRow,
+} from "mdbreact";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const makeLogin = async (e) => {
+    e.preventDefault();
+    const username = e.target.children[0].children[1].value;
+    const password = e.target.children[1].children[1].value;
+    const data = await fetch("http://localhost:9000/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include'
+    });
+  };
+
+  return (
+    <MDBCol md="5" xl="4">
+      <MDBCard id="classic-card">
+        <MDBCardBody className="black-text">
+          <h3 className="text-center"> Увійти в систему:</h3>
+          <hr className="hr-light" />
+          <form onSubmit={makeLogin}>
+            <MDBInput
+              className="black-text"
+              iconClass="black-text"
+              label="Ваш логін"
+              icon="user-alt"
+              value={username}
+              onChange={({ value }) => setUsername(value)}
+            />
+            <MDBInput
+              className="black-text"
+              iconClass="black-text"
+              label="Ваш пароль"
+              icon="lock"
+              type="password"
+              value={password}
+              onChange={({ value }) => setPassword(value)}
+            />
+            <div className="text-center mt-4 white-text d-flex flex-column justify-content-center">
+              <MDBCol className="text-center">
+                <MDBBtn color="black" className="white-text" type="submit">
+                  Увійти
+                </MDBBtn>
+              </MDBCol>
+              <MDBCol className="black-text text-center mt-3">
+                Немає облікового запису?{" "}
+                <a className="blue-text underline">
+                  <u>Створіть його!</u>
+                </a>
+              </MDBCol>
+            </div>
+          </form>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  );
+};
+
+export default Login;
---
+++ client/pages/auth.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import Login from "../components/login";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "../stylesheets/login.css";
+const Auth = () => {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.15.0/css/mdb.min.css" rel="stylesheet"></link>
+      </Head>
+      <div className="bg-login d-flex align-items-center justify-content-center">
+        <Login></Login>
+      </div>
+      </>
+  );
+};
+
+export default Auth;
