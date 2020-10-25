4cae40571ef19585e13bd8b311da8837b998dbe4
Zakkaray <bloomzakk@gmail.com>
2020-10-25 17:50:07 +0200

    [Tests] Fix test not working on actions

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -5,10 +5,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-console.log(process.env)
-
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
+
+console.log(isProduction);
 
 module.exports = { pool };
