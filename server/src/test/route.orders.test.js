73748ee70a187663df82a49c3fecc5ee123460ff
Zakkaray <bloomzakk@gmail.com>
2020-10-25 14:53:23 +0200

    fix Should be statement

--- server/src/test/route.orders.test.js
+++ server/src/test/route.orders.test.js
@@ -5,32 +5,32 @@ const app = require("../../app");
 chai.use(chaiHttp);
 
 describe("/GET orders/userOrders", () => {
-  it("Checks if token threw an error", (done) => {
-    chai
-      .request(app)
-      .get(`/orders/userOrders`)
-      .set("Cookie", `UID=${process.env.token};`)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.should.have
-          .property("text")
-          .that.eql("Invalid token, please log in again");
-        done();
-      });
-  });
+    it("Checks if token threw an error", (done) => {
+        chai
+            .request(app)
+            .get(`/orders/userOrders`)
+            .set("Cookie", `UID=${process.env.token};`)
+            .end((err, res) => {
+                res.status.should.be.equal(400);
+                res.should.have
+                    .property("text")
+                    .that.eql("Invalid token, please log in again");
+                done();
+            });
+    });
 });
 
 describe("/POST orders/createOrder", () => {
-  it("Checks if token threw an error", (done) => {
-    chai
-      .request(app)
-      .post(`/orders/createOrder`)
-      .end((err, res) => {
-        res.should.have.status(400);
-        res.should.have
-          .property("text")
-          .that.eql("Invalid token, please log in again");
-        done();
-      });
-  });
+    it("Checks if token threw an error", (done) => {
+        chai
+            .request(app)
+            .post(`/orders/createOrder`)
+            .end((err, res) => {
+                res.status.should.be.equal(400);
+                res.should.have
+                    .property("text")
+                    .that.eql("Invalid token, please log in again");
+                done();
+            });
+    });
 });
