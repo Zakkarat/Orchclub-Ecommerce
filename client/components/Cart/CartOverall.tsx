58ee3be26ec718eab71b302a67ec11a77e4b28e5
Borshosh Sashka <sashkaborshosh23@gmail.com>
2021-01-07 17:48:12 +0200

    feat(ui): improve card ui

--- client/components/Cart/CartOverall.tsx
+++ client/components/Cart/CartOverall.tsx
@@ -1,27 +1,33 @@
 import React from "react";
-import { MDBRow, MDBCol, MDBBtn } from "mdbreact";
-import {ICartOverallProps} from "../../interfaces/components/ICartProps";
+import { MDBRow, MDBCol, MDBBtn, MDBIcon } from "mdbreact";
+import { ICartOverallProps } from "../../interfaces/components/ICartProps";
 
-const CartOverall = ({ cart, initStateChange }:ICartOverallProps) => (
+const CartOverall = ({ cart, initStateChange }: ICartOverallProps) => (
   <MDBRow style={{ width: "40rem", marginTop: "2rem" }}>
     <MDBCol className="d-flex justify-content-start pr-3">
-      <h3 className="pt-2">
+      <h4 className="pt-2">
         Всего к оплате:{" "}
-        {cart.reduce((acc, curr) => {
-            if(curr.quantity) {
-                return acc + curr.quantity * curr.Price
+        <b>
+          {" "}
+          {cart.reduce((acc, curr) => {
+            if (curr.quantity) {
+              return acc + curr.quantity * curr.Price;
             }
             return acc;
-            }, 0)}
-      </h3>
+          }, 0)}{" "}
+          ₴{" "}
+        </b>
+      </h4>
     </MDBCol>
     <MDBCol className="d-flex justify-content-end">
       <MDBBtn
-        color="dark"
+        color="green"
         onClick={initStateChange}
         disabled={!cart.length}
         className="text-white font-input"
+        style={{ padding: " 10px 20px 10px 20px" }}
       >
+        <MDBIcon icon="dolly" className="pr-2" style={{ fontSize: "16px" }} />
         Продолжить
       </MDBBtn>
     </MDBCol>
